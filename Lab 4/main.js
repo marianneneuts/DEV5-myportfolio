b4a3ae315a7997527ba2c5d5e93269ab909d081a
Marianne <marianneneuts@gmail.com>
2022-10-13 17:34:41 +0200

    feat: add OrbitControls

--- Lab 4/main.js
+++ Lab 4/main.js
@@ -1,5 +1,22 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+camera.position.set( 0, 20, 100 );
+controls.update();
+
+function animate() {
+	requestAnimationFrame( animate );
+    controls.update();
+	renderer.render( scene, camera );
+}
+animate();
